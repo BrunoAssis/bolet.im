9f392e0ec7bc247849278969423f5eb4bcfe0c6a
Bruno Assis <brunoassis@gmail.com>
2013-11-07 16:05:42 -0200

    Changed Caching to File.

--- app/Config/core.php
+++ app/Config/core.php
@@ -312,7 +312,7 @@
  *       Please check the comments in bootstrap.php for more info on the cache engines available
  *       and their settings.
  */
-$engine = 'APC';
+$engine = 'File';
 
 // In development mode, caches should expire quickly.
 $duration = '+999 days';
