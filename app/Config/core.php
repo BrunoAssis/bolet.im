5e1a5be0b83990bddb1efd648eef31b2fe360ef6
Bruno Assis <brunoassis@gmail.com>
2013-11-07 15:07:20 -0200

    Deploy to PagodaBox

--- app/Config/core.php
+++ app/Config/core.php
@@ -312,7 +312,7 @@
  *       Please check the comments in bootstrap.php for more info on the cache engines available
  *       and their settings.
  */
-$engine = 'File';
+$engine = 'APC';
 
 // In development mode, caches should expire quickly.
 $duration = '+999 days';
